0040490789a7071482d5e36671831112a1db1b7c
Alex Pinto <alexfapa32@gmail.com>
2021-02-16 17:35:51 -0300

    Adicionado as classes Cliente, Endereço

--- src/main/java/com/example/salessystem/SalesSystemApplication.java
+++ src/main/java/com/example/salessystem/SalesSystemApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.salessystem.domain.Categoria;
 import com.example.salessystem.domain.Cidade;
+import com.example.salessystem.domain.Cliente;
+import com.example.salessystem.domain.Endereco;
 import com.example.salessystem.domain.Estado;
 import com.example.salessystem.domain.Produto;
+import com.example.salessystem.domain.enums.TipoCliente;
 import com.example.salessystem.repositories.CategoriaRepository;
 import com.example.salessystem.repositories.CidadeRepository;
+import com.example.salessystem.repositories.ClienteRepository;
+import com.example.salessystem.repositories.EnderecoRepository;
 import com.example.salessystem.repositories.EstadoRepository;
 import com.example.salessystem.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class SalesSystemApplication implements CommandLineRunner{
 	
 	@Autowired
 	EstadoRepository estadoRepository;
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@Autowired
+	EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesSystemApplication.class, args);
@@ -73,6 +84,18 @@ public class SalesSystemApplication implements CommandLineRunner{
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto. 20", "Jardim", "38228024", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "3877712", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+						
 	}
 
 }
---
+++ src/main/java/com/example/salessystem/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.example.salessystem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.salessystem.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}

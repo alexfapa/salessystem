6167fd9ba73f4acf86b08e71da3efc972cee940e
Alex Pinto <alexfapa32@gmail.com>
2021-02-17 16:56:01 -0300

    Adicionadas as classes de ItemPedido e ItemPedidoPK para geração de chave primária composta

--- src/main/java/com/example/salessystem/SalesSystemApplication.java
+++ src/main/java/com/example/salessystem/SalesSystemApplication.java
@@ -14,6 +14,7 @@ import com.example.salessystem.domain.Cidade;
 import com.example.salessystem.domain.Cliente;
 import com.example.salessystem.domain.Endereco;
 import com.example.salessystem.domain.Estado;
+import com.example.salessystem.domain.ItemPedido;
 import com.example.salessystem.domain.Pagamento;
 import com.example.salessystem.domain.PagamentoComBoleto;
 import com.example.salessystem.domain.PagamentoComCartao;
@@ -26,6 +27,7 @@ import com.example.salessystem.repositories.CidadeRepository;
 import com.example.salessystem.repositories.ClienteRepository;
 import com.example.salessystem.repositories.EnderecoRepository;
 import com.example.salessystem.repositories.EstadoRepository;
+import com.example.salessystem.repositories.ItemPedidoRepository;
 import com.example.salessystem.repositories.PagamentoRepository;
 import com.example.salessystem.repositories.PedidoRepository;
 import com.example.salessystem.repositories.ProdutoRepository;
@@ -56,6 +58,9 @@ public class SalesSystemApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesSystemApplication.class, args);
@@ -130,6 +135,19 @@ public class SalesSystemApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip3));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 						
 	}
 

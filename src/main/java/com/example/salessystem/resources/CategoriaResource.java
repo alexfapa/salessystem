593d9824de1c5efdb73bef736280c7d8906d9c77
Alex Pinto <alexfapa32@gmail.com>
2021-02-18 12:37:55 -0300

    Adição do método update Categorias através do PUT

--- src/main/java/com/example/salessystem/resources/CategoriaResource.java
+++ src/main/java/com/example/salessystem/resources/CategoriaResource.java
@@ -2,7 +2,6 @@ package com.example.salessystem.resources;
 
 
 import java.net.URI;
-import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +26,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
@@ -37,5 +36,12 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }

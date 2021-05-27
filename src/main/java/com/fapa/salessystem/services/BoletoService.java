d45128bc4481534f947b4ed6ee874396da825587
Alex Pinto <francisco.alex@meta.com.br>
2021-05-27 19:43:28 -0300

    Correção

---
+++ src/main/java/com/fapa/salessystem/services/BoletoService.java
@@ -0,0 +1,18 @@
+package com.fapa.salessystem.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.fapa.salessystem.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pgto, Date instantePedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instantePedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pgto.setDataPagamento(cal.getTime());
+	}
+}

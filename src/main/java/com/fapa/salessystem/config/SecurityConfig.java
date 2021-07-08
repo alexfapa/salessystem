32e4813508d0d3f0c179c50830cfb8647095765c
Alex Pinto <francisco.alex@meta.com.br>
2021-07-07 23:05:54 -0300

    Adicionando regras de autorização de métodos

--- src/main/java/com/fapa/salessystem/config/SecurityConfig.java
+++ src/main/java/com/fapa/salessystem/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,11 +20,13 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.fapa.salessystem.security.JWTAuthenticationFilter;
+import com.fapa.salessystem.security.JWTAuthorizationFilter;
 import com.fapa.salessystem.security.JwtUtil;
 
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
@@ -42,9 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
+			"/categorias/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
 			"/clientes/**"
-			
 	};
 	
 	@Override
@@ -58,10 +63,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.cors().and().csrf().disable(); //desabititado contra ataques csrf
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() //esta linha permite somente os métodos GET na string PUBLIC_MATCHERS_GET
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll() //esta linha permite todas as operações dos métodos na string PUBLIC_MATCHERS
 			.anyRequest().authenticated(); //esta linha pede permissão a todos os demais endpoints
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDatailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //esta chamada impede que seja criado sessões de usuário
 	}
 	

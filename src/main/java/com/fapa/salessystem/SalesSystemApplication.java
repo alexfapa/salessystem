e90ceef21963948f665ac94c73a4f382869fb559
Alex Pinto <francisco.alex@meta.com.br>
2021-07-19 23:45:32 -0300

    Inserindo imagens no AWS S3

--- src/main/java/com/fapa/salessystem/SalesSystemApplication.java
+++ src/main/java/com/fapa/salessystem/SalesSystemApplication.java
@@ -2,12 +2,18 @@ package com.fapa.salessystem;
 
 import java.text.ParseException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fapa.salessystem.services.S3Service;
+
 @SpringBootApplication
 public class SalesSystemApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesSystemApplication.class, args);
@@ -15,8 +21,7 @@ public class SalesSystemApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws ParseException {
-		
-						
+		s3Service.uploadFile("C:\\Users\\francisco.alex\\Pictures\\Photos\\DudaRanataEu.jpg");						
 	}
 
 }

df8e1fe676e823e1da3e9a7abffb9302441eba03
Alex Pinto <francisco.alex@meta.com.br>
2021-07-28 00:11:16 -0300

    Usando padrão de nomes para imagens

---
+++ src/main/java/com/fapa/salessystem/services/ImageService.java
@@ -0,0 +1,56 @@
+package com.fapa.salessystem.services;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fapa.salessystem.services.exceptions.FileException;
+
+@Service
+public class ImageService {
+	
+	public BufferedImage getJpgImageFromFile(MultipartFile uploadFile) {
+		String ext = FilenameUtils.getExtension(uploadFile.getOriginalFilename()); //pegar a extensão do arquivo
+		
+		if(!"png".equals(ext) && !"jpg".equals(ext)) {
+			throw new FileException("Somente imagens do tipo PNG ou JPG são permitidas");
+		}
+		
+		try {
+			BufferedImage img = ImageIO.read(uploadFile.getInputStream());
+			if("png".equals(ext)) {
+				img = pngToJpg(img);
+			}
+			return img;
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler arquivo!");
+		}
+	}
+
+	//método para converter imagem PNG em JPG
+	public BufferedImage pngToJpg(BufferedImage img) {
+		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(),
+				BufferedImage.TYPE_INT_RGB);
+		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE,null);
+		return jpgImage;
+	}
+	
+	public InputStream getInputStram(BufferedImage img, String extension) {
+		try {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			ImageIO.write(img, extension, os);
+			return new ByteArrayInputStream(os.toByteArray());
+		} catch (IOException e) {
+			throw new FileException("Erro ao ler arquivo");
+		}
+	}
+}
